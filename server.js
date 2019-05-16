f12cdf7370c247fc0823f9eb869fdc0387cd9e43
Dave <dave.e.siegel@gmail.com>
2019-05-16 16:02:12 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const socketio = require('socket.io');
+
+const port = process.env.PORT || 3000;
+
+app.get('/app.js', (req, res, next)=> res.sendFile(path.join(__dirname, 'dist', 'main.js')));
+
+app.get('/', (req, res, next)=> res.sendFile(path.join(__dirname, 'index.html')));
+
+const server = app.listen(port, ()=> console.log(`listening on port ${port}`));
+
+const io = socketio(server);
+
+io.on('connect', socket => socket.on('stateUpdate', color => {
+    socket.broadcast.emit('stateUpdate', color);   
+}));
