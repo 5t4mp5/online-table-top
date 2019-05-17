e4cb9c25b639c201f83b901a856c720967a7560a
Dave <dave.e.siegel@gmail.com>
2019-05-17 13:08:42 -0400

    set session on user when they connect, if they do not already have session

--- server.js
+++ server.js
@@ -1,21 +1,41 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
-const socketio = require('socket.io');
+const session = require("express-session");
+const path = require("path");
+const socketio = require("socket.io");
+const uuidV4 = require("uuid/v4");
 
 const port = process.env.PORT || 3000;
 
-app.get('/app.js', (req, res, next)=> res.sendFile(path.join(__dirname, 'dist', 'main.js')));
+app.use(
+  session({
+    secret: "This is not a very secure secret...",
+    resave: false,
+    saveUninitialized: false
+  })
+);
 
-app.get('/', (req, res, next)=> res.sendFile(path.join(__dirname, 'index.html')));
+app.get("/app.js", (req, res, next) =>
+  res.sendFile(path.join(__dirname, "dist", "main.js"))
+);
 
-const server = app.listen(port, ()=> console.log(`listening on port ${port}`));
+app.get("/", (req, res, next) => {
+  if (!req.session.userId) {
+    console.log("NO SESSION");
+    req.session.userId = uuidV4();
+  } else {
+      console.log("SESSION EXISTS: ", req.session.userId);
+  }
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+const server = app.listen(port, () => console.log(`listening on port ${port}`));
 
 const io = socketio(server);
 
-io.on('connect', socket => {
-    socket.on('stateUpdate', color => {
-        "STATE UPDATE";
-        socket.broadcast.emit('stateUpdate', color);   
-    });
+io.on("connect", socket => {
+  socket.on("stateUpdate", color => {
+    "STATE UPDATE";
+    socket.broadcast.emit("stateUpdate", color);
+  });
 });
